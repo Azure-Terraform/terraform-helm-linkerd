6adb375fc87320c53eae29bbce067e67bfad0199
Gary Golub <gary.golub@lexisnexisrisk.com>
2022-10-25 14:46:09 -0400

    Fixed namespaces for linkerd-viz

--- main.tf
+++ main.tf
@@ -1,21 +1,42 @@
 resource "time_static" "cert_create_time" {}
 
-# create namespaces for linkerd and any extensions (linkerd-viz or linkerd-jaeger)
-resource "kubernetes_namespace" "namespace" {
-  for_each = toset(local.namespaces)
+resource "kubernetes_namespace" "linkerd" {
   metadata {
-    name        = each.key
+    name        = var.chart_namespace
     annotations = { "linkerd.io/inject" = "disabled" }
-    labels      = {
-      "linkerd.io/extension" = trimprefix(each.key, "linkerd-")
-      "linkerd.io/is-control-plane" = "true"
-      "linkerd.io/control-plane-ns" = each.key
+    labels = {
+      "linkerd.io/is-control-plane"          = "true"
       "config.linkerd.io/admission-webhooks" = "disabled"
+      "linkerd.io/control-plane-ns"          = "linkerd"
     }
   }
 }
 
+resource "kubernetes_namespace" "linkerd-viz" {
+  count = contains(var.extensions, "viz") ? 1 : 0
+
+  metadata {
+    name        = "linkerd-viz"
+    annotations = { "linkerd.io/inject" = "enabled" }
+    labels      = { "linkerd.io/extension" = "viz" }
+  }
+}
+
+resource "kubernetes_namespace" "linkerd-jaeger" {
+  count = contains(var.extensions, "jaeger") ? 1 : 0
+
+  metadata {
+    annotations = {
+      "linkerd.io/inject"             = "enabled"
+      "config.linkerd.io/proxy-await" = "enabled"
+    }
+    labels = { "linkerd.io/extension" = "jaeger" }
+  }
+}
+
 resource "helm_release" "crds" {
+  depends_on = [kubernetes_namespace.linkerd]
+
   name             = "linkerd"
   chart            = "linkerd-crds"
   namespace        = var.chart_namespace
@@ -25,11 +46,11 @@ resource "helm_release" "crds" {
   atomic           = var.atomic
   create_namespace = false
   devel            = true
-
-  depends_on = [kubernetes_namespace.namespace]
 }
 
 module "issuer" {
+  depends_on = [helm_release.crds]
+
   source = "./modules/issuer"
 
   namespace  = var.chart_namespace
@@ -38,12 +59,10 @@ module "issuer" {
   trust_anchor_validity_hours = var.trust_anchor_validity_hours
   issuer_validity_hours       = var.issuer_validity_hours
   ca_cert_expiration_hours    = var.ca_cert_expiration_hours
-
-  depends_on = [helm_release.crds]
 }
 
 resource "helm_release" "cni" {
-  depends_on = [kubernetes_namespace.namespace]
+  depends_on = [kubernetes_namespace.linkerd]
 
   count = var.cni_enabled ? 1 : 0
 
@@ -58,15 +77,17 @@ resource "helm_release" "cni" {
 }
 
 resource "helm_release" "control_plane" {
-  name       = "linkerd-control-plane"
-  chart      = "linkerd-control-plane"
-  namespace  = var.chart_namespace
+  depends_on = [helm_release.cni, module.issuer]
+
+  name             = "linkerd-control-plane"
+  chart            = "linkerd-control-plane"
+  namespace        = var.chart_namespace
   create_namespace = false
-  repository = "https://helm.linkerd.io/edge"
-  version    = "1.9.3-edge"
-  timeout    = var.chart_timeout
-  atomic     = var.atomic
-  devel      = true
+  repository       = "https://helm.linkerd.io/edge"
+  version          = "1.9.3-edge"
+  timeout          = var.chart_timeout
+  atomic           = var.atomic
+  devel            = true
 
   set_sensitive {
     name  = "identityTrustAnchorsPEM"
@@ -93,11 +114,11 @@ resource "helm_release" "control_plane" {
     var.ha_enabled ? [file("${path.module}/templates/control-plane-ha.yaml")] : [],
     [yamlencode(local.linkerd), var.additional_yaml_config]
   )
-
-  depends_on = [helm_release.cni, module.issuer]
 }
 
 resource "helm_release" "extension" {
+  depends_on = [helm_release.control_plane]
+
   for_each = var.extensions
 
   name      = "linkerd-${each.key}"
@@ -126,7 +147,5 @@ resource "helm_release" "extension" {
   }
 
   values = lookup(local.extension_values, each.key, [])
-
-  depends_on = [helm_release.control_plane]
 }
 
